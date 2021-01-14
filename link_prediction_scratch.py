3e63ed5f7d50b46c2ba843826112679e14d4865a
Carlos <carloslago@opendeusto.es>
2021-01-14 19:54:04 +0100

    Link prediction update

--- link_prediction_scratch.py
+++ link_prediction_scratch.py
@@ -13,13 +13,12 @@ embeddings_file = 'blogcatalog.embeddings'
 nodes1 = []
 nodes2 = []
 all_nodes = {}
-edges = []
+edges = {}
 annot = loadmat(dataset)
 net = annot['network']
 cx = net.tocoo()
 cont = 0
 for i, j, v in tqdm(zip(cx.row, cx.col, cx.data)):
-    if cont==330000: break
     nodes1.append(i)
     if i not in all_nodes:
         all_nodes[i] = 1
@@ -30,7 +29,7 @@ for i, j, v in tqdm(zip(cx.row, cx.col, cx.data)):
         all_nodes[j] = 1
     else:
         all_nodes[j] += 1
-    edges.append([i, j])
+    edges[hash(str(i)+str(j))] = 0
     cont += 1
 
 
@@ -49,23 +48,23 @@ for i in tqdm(range(len(nodes1))):
         nodes1[i] = None
         nodes2[i] = None
 
+
 nodes_1_train = list(filter(None, nodes1))
 nodes_2_train = list(filter(None, nodes2))
 
 y_test = [1 for i in nodes_1_test]
 y_train = [1 for i in nodes_1_train]
-
 for i in tqdm(range(len(nodes_1_train))):
-    negative_edge = edges[0]
-    while negative_edge[0] in all_nodes.keys() or negative_edge[1] in all_nodes.keys():
+    negative_edge = [0, 0]
+    while hash(str(negative_edge[0])+str(negative_edge[1])) in edges or negative_edge==[0, 0]:
         negative_edge = [random.randint(0, len(all_nodes)), random.randint(0, len(all_nodes))]
     nodes_1_train.append(negative_edge[0])
     nodes_2_train.append(negative_edge[1])
     y_train.append(0)
 
 for i in tqdm(range(len(nodes_1_test))):
-    negative_edge = edges[0]
-    while negative_edge[0] in all_nodes.keys() or negative_edge[1] in all_nodes.keys():
+    negative_edge = [0, 0]
+    while hash(str(negative_edge[0])+str(negative_edge[1])) in edges or negative_edge==[0, 0]:
         negative_edge = [random.randint(0, len(all_nodes)), random.randint(0, len(all_nodes))]
     nodes_1_test.append(negative_edge[0])
     nodes_2_test.append(negative_edge[1])
@@ -77,8 +76,25 @@ print("To embedding")
 
 model = KeyedVectors.load_word2vec_format(embeddings_file, binary=False)
 
-x_train = [([1/(1+ np.exp(-np.dot(model[str(nodes_1_train[i])], model[str(nodes_2_train[i])])))]) for i in range(len(nodes_1_train))]
-x_test = [([1/(1+ np.exp(-np.dot(model[str(nodes_1_test[i])], model[str(nodes_2_test[i])])))]) for i in range(len(nodes_1_test))]
+
+x_train = []
+x_test = []
+for i in range(len(nodes_1_train)):
+    try:
+        x_t = ([1/(1+ np.exp(-np.dot(model[str(nodes_1_train[i])], model[str(nodes_2_train[i])])))])
+        x_train.append(x_t)
+    except:
+        del y_train[i]
+
+for i in range(len(nodes_1_test)):
+    try:
+        x_t = ([1/(1+ np.exp(-np.dot(model[str(nodes_1_test[i])], model[str(nodes_2_test[i])])))])
+        x_test.append(x_t)
+    except:
+        del y_test[i]
+
+# x_train = [([1/(1+ np.exp(-np.dot(model[str(nodes_1_train[i])], model[str(nodes_2_train[i])])))]) for i in range(len(nodes_1_train))]
+# x_test = [([1/(1+ np.exp(-np.dot(model[str(nodes_1_test[i])], model[str(nodes_2_test[i])])))]) for i in range(len(nodes_1_test))]
 
 x_train = np.array(x_train)
 x_test = np.array(x_test)
