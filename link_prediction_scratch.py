e5e9b97f2c45fea9a2b4ac969aaf2523b86eac1f
Carlos <carloslago@opendeusto.es>
2021-01-14 20:49:15 +0100

    Fixed nodes array size

--- link_prediction_scratch.py
+++ link_prediction_scratch.py
@@ -8,6 +8,24 @@ import numpy as np
 from tqdm import tqdm
 import csv
 
+
+def directed(edges, n_nodes, prob):
+    if random.uniform(0, 1) < prob:
+        negative_edge = random.choice(list(edges.values()))[::-1]
+        while hash(str(negative_edge[0]) + str(negative_edge[1])) in edges or negative_edge == [0, 0]:
+            negative_edge = random.choice(list(edges.values()))[::-1]
+    else:
+        negative_edge = [0, 0]
+        while hash(str(negative_edge[0]) + str(negative_edge[1])) in edges or negative_edge == [0, 0]:
+            negative_edge = [random.randint(0, n_nodes), random.randint(0, n_nodes)]
+    return negative_edge
+
+def undirected(edges, n_nodes):
+    negative_edge = [0, 0]
+    while hash(str(negative_edge[0]) + str(negative_edge[1])) in edges or negative_edge == [0, 0]:
+        negative_edge = [random.randint(0, n_nodes), random.randint(0, n_nodes)]
+    return negative_edge
+
 dataset = 'data/blogcatalog.mat'
 embeddings_file = 'blogcatalog.embeddings'
 nodes1 = []
@@ -20,16 +38,16 @@ cx = net.tocoo()
 cont = 0
 for i, j, v in tqdm(zip(cx.row, cx.col, cx.data)):
     nodes1.append(i)
+    nodes2.append(j)
     if i not in all_nodes:
         all_nodes[i] = 1
     else:
         all_nodes[i] += 1
-    nodes2.append(j)
     if j not in all_nodes:
         all_nodes[j] = 1
     else:
         all_nodes[j] += 1
-    edges[hash(str(i)+str(j))] = 0
+    edges[hash(str(i)+str(j))] = [i, j]
     cont += 1
 
 
@@ -38,34 +56,29 @@ test_size = round(len(nodes1)*0.5)
 
 nodes_1_test = []
 nodes_2_test = []
-
+nodes_1_train = []
+nodes_2_train = []
 for i in tqdm(range(len(nodes1))):
     node = nodes1[i]
     node2 = nodes2[i]
     if all_nodes[node]>1 and all_nodes[node2]>1 and len(nodes_1_test)<=test_size:
         nodes_1_test.append(node)
         nodes_2_test.append(node2)
-        nodes1[i] = None
-        nodes2[i] = None
-
-
-nodes_1_train = list(filter(None, nodes1))
-nodes_2_train = list(filter(None, nodes2))
+    else:
+        if node is None or nodes2 is None: print("hello")
+        nodes_1_train.append(node)
+        nodes_2_train.append(nodes2)
 
 y_test = [1 for i in nodes_1_test]
 y_train = [1 for i in nodes_1_train]
 for i in tqdm(range(len(nodes_1_train))):
-    negative_edge = [0, 0]
-    while hash(str(negative_edge[0])+str(negative_edge[1])) in edges or negative_edge==[0, 0]:
-        negative_edge = [random.randint(0, len(all_nodes)), random.randint(0, len(all_nodes))]
+    negative_edge = undirected(edges, len(all_nodes))
     nodes_1_train.append(negative_edge[0])
     nodes_2_train.append(negative_edge[1])
     y_train.append(0)
 
 for i in tqdm(range(len(nodes_1_test))):
-    negative_edge = [0, 0]
-    while hash(str(negative_edge[0])+str(negative_edge[1])) in edges or negative_edge==[0, 0]:
-        negative_edge = [random.randint(0, len(all_nodes)), random.randint(0, len(all_nodes))]
+    negative_edge = undirected(edges, len(all_nodes))
     nodes_1_test.append(negative_edge[0])
     nodes_2_test.append(negative_edge[1])
     y_test.append(0)
